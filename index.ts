c8459888e7d741124420fbe0970906a8850d5d78
blissito <blissitos@gmail.com>
2025-12-11 09:05:31 -0600

    4o-mini

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import "dotenv/config";
 import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
 
-const model = openai("gpt-4.1-mini");
+const model = openai("gpt-4o-mini");
 
 const system =
-  "Eres un robot inteligente que asiste con lo que le piden sin romper las 3 leyes de la robótica de isaac asimov";
+  "Eres un robot inteligente que asiste con lo que le piden sin romper las 3 leyes de la robï¿½tica de isaac asimov";
 
 export const chat = (prompt: string) =>
   streamText({
